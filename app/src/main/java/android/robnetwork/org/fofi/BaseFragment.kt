574f5aaafa0ea5d508b55a9d891692819341506c
Rob <guisalmon@gmail.com>
2018-11-05 16:15:30 +0100

    Hello world

---
+++ app/src/main/java/android/robnetwork/org/fofi/BaseFragment.kt
@@ -0,0 +1,39 @@
+package android.robnetwork.org.fofi
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.os.Parcelable
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import com.airbnb.mvrx.BaseMvRxFragment
+import com.airbnb.mvrx.MvRx
+
+abstract class BaseFragment<B: ViewDataBinding>: BaseMvRxFragment() {
+    @get:LayoutRes
+    abstract val layoutRes: Int
+
+    var binding: B? = null
+        private set
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding?.let { setupUI(it) }
+        return binding?.root
+    }
+
+    open fun setupUI(binding: B) {}
+
+    protected fun navigateTo(navId: Int, args: Parcelable) {
+        val  bundle = Bundle()
+        bundle.putParcelable(MvRx.KEY_ARG, args)
+        NavHostFragment.findNavController(this).navigate(navId, bundle)
+    }
+
+    protected fun navigateTo(navId: Int) {
+        NavHostFragment.findNavController(this).navigate(navId)
+    }
+}

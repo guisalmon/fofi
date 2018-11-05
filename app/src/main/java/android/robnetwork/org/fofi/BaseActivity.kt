2764cbcb54043e4840c5fad8667839eefec8f04c
Rob <guisalmon@gmail.com>
2018-11-05 15:10:01 +0100

    Initial commit

---
+++ app/src/main/java/android/robnetwork/org/fofi/BaseActivity.kt
@@ -0,0 +1,23 @@
+package android.robnetwork.org.fofi
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import com.airbnb.mvrx.BaseMvRxActivity
+
+abstract class BaseActivity<B : ViewDataBinding> : BaseMvRxActivity() {
+    @get:LayoutRes
+    abstract val layoutRes: Int
+
+    var binding: B? = null
+        private set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding?.let { setupUI(it) }
+    }
+
+    open fun setupUI(binding: B) {}
+}
